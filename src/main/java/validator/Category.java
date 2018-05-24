1d2d5a2efe26bbf648a05f385ba1c12fc3dc5a6d
Andon <jxia616@aucklanduni.ac.nz>
2018-05-24 18:57:07 +1200

    Added implementation for a Document and Categories for the documents. Includes example setup for a mock document and using a document builder.

--- src/main/java/validator/Category.java
+++ src/main/java/validator/Category.java
@@ -1,13 +1,37 @@
 package validator;
 
-public class Category {
+import java.util.List;
 
+import MarketComprehension.Document;
+
+public class Category {
+	public enum CategoryType {SPORTS, ANIMALS ,WALKING,ARTS,CRAFTS,COLLECTIBLES,
+		BEAUTY, FRAGRANCES,BOOKS,MAGAZINES,CLOTHING, COMPUTERS, ACCESSORIES,SHOES,
+		EDUCATION, ELECTRONICS, ENTERTAINMENT, FOOD, DRINK};
+		
+	private List<Document> documents;
+	
+	public Category(List<Document> documents) {
+		this.documents = documents;
+	}
 	/**
 	 * Return number of documents in this category
 	 * @return documents in this category
 	 */
 	public int size() {
 		return 0;
+	}
+	
+	
+	
+	public int size(Category.CategoryType categoryType) {
+		int counter = 0;
+		for (Document document : documents) {
+			if (document.getCategories().contains(categoryType)) {
+				counter ++;
+			}
+		}
+		return counter;
 	}
 
 }
