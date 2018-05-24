c3223057038924700c0dfff616c318375d31a58c
whattupjellycup <gman485@aucklanduni.ac.nz>
2018-05-24 15:22:29 +1200

    created bdd test cases for setting the relevance of a category

--- src/test/java/validator/StepsChangeSetCategoryRelevance.java
+++ src/test/java/validator/StepsChangeSetCategoryRelevance.java
@@ -1,5 +1,33 @@
 package validator;
 
-public class StepsChangeSetCategoryRelevance {
+import org.jbehave.core.annotations.BeforeScenario;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.When;
+import org.jbehave.core.annotations.Then;
+import org.mockito.Mockito;
+import static org.junit.Assert.*;
 
+public class StepsChangeSetCategoryRelevance {
+	private final static double EPSILON = 0.001;
+	Category _c1;
+	Cluster _cluster;
+	
+	@Given("that the user is given a cluster of categories: $c1")
+	public void givenCategories(String c1){
+		_cluster = new Cluster();
+		_c1 = new Category(c1);
+		_cluster.add(_c1);
+	}
+	
+	@When("the user sets the relevance of $category to $relevance")
+	public void whenRelevanceSet(String category, double relevance){
+		Cluster c = _cluster.get(category);
+		c.relevance(relevance);
+	}
+	
+	
+	@Then("the relevance of $category changes to $setRelevance")
+	public void thenRelevanceChanges(String category, double setRelevance){
+		assertEquals(setRelevance, _cluster.get(category).relevance, EPSILON);
+	}
 }
--- src/test/resources/validator/test_set_category_relevance.story
+++ src/test/resources/validator/test_set_category_relevance.story
@@ -4,6 +4,10 @@ As a user
 I want to change the relevance of categories given to me
 					 
 Scenario:  User changes the relevance of different categories
-Given that the user is given a cluster of categories
-When the user sets the relevance of a category
-Then the relevance of category changes
+Given that the user is given a cluster of categories: <c1>
+When the user sets the relevance of <category> to <relevance>
+Then the relevance of <category> changes to <setRelevance>
+
+Examples:
+|c1|category|relevance|setRelevance|
+|Dog|Dog|0.2|0.2|
