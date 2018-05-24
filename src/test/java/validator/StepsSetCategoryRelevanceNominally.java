7fb6cf3b8bbb55260f7ac2458f14216c6c74d32b
whattupjellycup <gman485@aucklanduni.ac.nz>
2018-05-24 21:17:30 +1200

    implemented setting nominal relevance

--- src/test/java/validator/StepsSetCategoryRelevanceNominally.java
+++ src/test/java/validator/StepsSetCategoryRelevanceNominally.java
@@ -10,15 +10,33 @@ public class StepsSetCategoryRelevanceNominally {
 	private final static double EPSILON = 0.001;
 	Category _category;
 	
-	@Given("that the user is given a category: $category")
+	@Given("that the user is given a cluster with the category: $category")
 	public void givenCategory(String category){
 		_category = new Category(category);
 	}
 	@When("the user sets the relevance of category to $nominalRelevance")
-	public void whenSetNominalRelevance(Relevance nominalRelevance){
-		_category.relevance(nominalRelevance);
+	public void whenSetNominalRelevance(String nominalRelevance){
+		Relevance r = null;
+		switch(nominalRelevance){
+		case "NOT_RELEVANT":
+			r = Relevance.NOT_RELEVANT;
+			break;
+		case "WEAK_RELEVANT":
+			r = Relevance.WEAK_RELEVANT;
+			break;
+		case "RELEVANT":
+			r = Relevance.RELEVANT;
+			break;
+		case "VERY_RELEVANT":
+			r = Relevance.VERY_RELEVANT;
+			break;
+		case "THE_SAME":
+			r = Relevance.THE_SAME;
+			break;
+		}
+		_category.relevance(r);
 	}
-	@Then("the relevance of category changes to $relevance")
+	@Then("the relevance of the category changes to $relevance")
 	public void thenRevenceIsSetTo(double relevance){
 		assertEquals(relevance, _category.relevance(), EPSILON);
 	}
--- src/test/java/validator/testsuite/TestSuite.java
+++ src/test/java/validator/testsuite/TestSuite.java
@@ -5,12 +5,14 @@ import org.junit.runners.Suite;
 
 import validator.TestPopularityCalculation;
 import validator.TestSetCategoryRelevance;
+import validator.TestSetCategoryRelevanceNominally;
 
 @RunWith(Suite.class)				
 @Suite.SuiteClasses({
 	//add test cases here
 	TestPopularityCalculation.class,
 	TestSetCategoryRelevance.class,
+	TestSetCategoryRelevanceNominally.class,
 })
 public class TestSuite {
 
