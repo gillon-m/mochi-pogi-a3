195ce853f35679b02a99d40367aeafb529893466
CJ Bang <bchj513@hotmail.com>
2018-05-24 16:25:59 +1200

    Business idea keyword extraction (#7)
    * Added a user story for changing weight when user passes in a valid weight
    
    * Added InputProcessor interface and Word class
    
    * Added a test case for changing when user input is valid, implemented KeywordsEditor and Word classes
    
    * Added a user story when user input is invalid, implemented exception handling logic in KeywordsEditor and added KeywordException

---
+++ src/main/java/validator/exceptions/KeywordException.java
@@ -0,0 +1,23 @@
+package validator.exceptions;
+
+@SuppressWarnings("serial")
+public class KeywordException extends RuntimeException {
+	private String _message;
+	
+	public KeywordException(String message) {
+		_message = message;
+	}
+	
+	public String getMessage() {
+		return _message;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof KeywordException 
+				&& _message == ((KeywordException) other)._message) {
+			return true;
+		}
+		return false;
+	}
+}
