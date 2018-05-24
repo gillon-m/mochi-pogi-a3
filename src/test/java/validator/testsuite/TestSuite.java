5e86403d2b7c96e618bd99b4fb71510a156ebc23
CJ Bang <bchj513@hotmail.com>
2018-05-24 20:48:50 +1200

    Business idea keyword extraction (#11)
    * Added test cases to test suite
    
    * Added a user story for injecting keyword when the keywords is full and implemented logic for that

--- src/test/java/validator/testsuite/TestSuite.java
+++ src/test/java/validator/testsuite/TestSuite.java
@@ -3,6 +3,11 @@ package validator.testsuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import validator.TestChangeWeightInvalid;
+import validator.TestChangeWeightValid;
+import validator.TestInjectKeywordInvalid;
+import validator.TestInjectKeywordValid;
+import validator.TestInjectKeywordWhenFull;
 import validator.TestPopularityCalculation;
 import validator.TestSetCategoryRelevance;
 
@@ -11,6 +16,11 @@ import validator.TestSetCategoryRelevance;
 	//add test cases here
 	TestPopularityCalculation.class,
 	TestSetCategoryRelevance.class,
+	TestInjectKeywordInvalid.class,
+	TestInjectKeywordValid.class,
+	TestInjectKeywordWhenFull.class,
+	TestChangeWeightInvalid.class,
+	TestChangeWeightValid.class
 })
 public class TestSuite {
 
