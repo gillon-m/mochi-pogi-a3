8de6c4a072cea993b210ea7ac48a81be3df6e5d6
chaejj <bchj513@hotmail.com>
2018-05-26 13:03:13 +1200

    Included a missed test case into the test suite and deleted a unnecessary steps file

--- src/test/java/validator/testsuite/TestSuite.java
+++ src/test/java/validator/testsuite/TestSuite.java
@@ -3,6 +3,7 @@ package validator.testsuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import validator.BusinessIdeaMaturityTest;
 import validator.ChangeKeywordWeightInvalidTest;
 import validator.ChangeKeywordWeightValidTest;
 import validator.InjectKeywordInvalidTest;
@@ -18,6 +19,7 @@ import validator.SetCategoryRelevanceNominallyTest;
 @RunWith(Suite.class)				
 @Suite.SuiteClasses({
 	//add test cases here
+	BusinessIdeaMaturityTest.class,
 	PopularityCalculationTest.class,
 	SetCategoryRelevanceTest.class,
 	SetCategoryRelevanceNominallyTest.class,
