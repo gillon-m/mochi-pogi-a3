e6d00a9d731cbc0ea72f0527dc8ce19a519b1c4c
chaejj <bchj513@hotmail.com>
2018-05-26 12:06:08 +1200

    Removed steps files and moved the code to test case file (for better readability and consistency in package structure)

--- src/test/java/validator/RemoveKeywordWhenEmptyTest.java
+++ src/test/java/validator/RemoveKeywordWhenEmptyTest.java
@@ -1,22 +1,64 @@
 package validator;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.junit.JUnitStory;
 import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.StoryReporterBuilder;
-import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.CandidateSteps;
 import org.jbehave.core.steps.InstanceStepsFactory;
 import org.junit.Test;
+import org.mockito.Mockito;
 
-import validator.steps.RemoveKeywordWhenEmptySteps;
+import validator.exceptions.KeywordException;
 
 public class RemoveKeywordWhenEmptyTest extends JUnitStory {
-	public InjectableStepsFactory stepsFactory() {
-		return new InstanceStepsFactory(configuration(), new RemoveKeywordWhenEmptySteps());
+	private InputProcessor _processor;
+	private KeywordsEditor _editor;
+	private Set<Word> _words;
+	private Word _word;
+	private Throwable _exception;
+
+	@Given("An empty keywords list")
+	public void givenEmptyKeywordsList() {
+		_processor = Mockito.mock(InputProcessor.class);
+		Set<Word> words = new HashSet<Word>();
+		
+		Mockito.when(_processor.extractKeywords()).thenReturn(words);	
+		_words = _processor.extractKeywords();
+	}
+
+	@When("User selects a word $word to remove from the list")
+	public void whenUserRemovesKeywordFromEmptyList(String word) {
+		_editor = new KeywordsEditor(_words);
+		_word = Mockito.spy(new Word(word));
+		try {
+			_editor.removeWord(_word);			
+		} catch (KeywordException e) {
+			_exception = e;
+		}
 	}
 	
+	@Then("User gets an error message $message")
+	public void thenUserGetsErrorMessage(String message) {
+		assertEquals(message, _exception.getMessage());
+	}
+
+	@Override
+	public List<CandidateSteps> candidateSteps() {
+		return new InstanceStepsFactory(configuration(), this).createCandidateSteps();
+	}
+
 	@Override
 	public Configuration configuration() {
 		return new MostUsefulConfiguration().useStoryLoader(
