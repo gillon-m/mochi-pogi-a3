7fee379df93dd031bd463960580bc84fb63177da
jxturtle <jxia616@aucklanduni.ac.nz>
2018-05-27 18:19:01 +1200

    Changed database to an interface implementation, and mocks created off the interface. (#25)

--- src/test/java/validator/MarketComprehensionTest.java
+++ src/test/java/validator/MarketComprehensionTest.java
@@ -17,6 +17,7 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.Matchers.hasProperty;
 
 import validator.marketcomprehension.Document;
+import validator.marketcomprehension.DocumentBuilder;
 import validator.marketcomprehension.DocumentPersistence;
 import validator.marketcomprehension.MarketComprehension;
 import validator.marketcomprehension.MongoClient;
@@ -37,11 +38,68 @@ public class MarketComprehensionTest {
 		MockitoAnnotations.initMocks(this);
 		documentRegistryDBName = "DocumentRegistry";
 		mongoClient = Mockito.mock(MongoClient.class);
-		mongoDatabase = new MongoDatabase();
+		mongoDatabase = Mockito.mock(MongoDatabase.class);
+		List<Document> documents = createDocumentMocks();
 		Mockito.doReturn(mongoDatabase).when(mongoClient).getDatabase(documentRegistryDBName);
-
-
+		Mockito.doReturn(documents).when(mongoDatabase).getList();
+		
 	}
+		
+		
+		private List<Document> createDocumentMocks() {
+			Document dogWalkingRoskillDocument = new DocumentBuilder()
+					.setTitle("Dog walking in east Auckland")
+					.setKeywords(new ArrayList<Word>(Arrays.asList(new Word("Dogs"), new Word("Walking"))))
+					.setSummary("An example of a summary for dog walking in west Auckland")
+					.setContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+					.buildDocument();
+			
+			Document animalDayCareDocument = new DocumentBuilder()
+					.setTitle("Animal daycare services in central Auckland")
+					.setKeywords(new ArrayList<Word>(Arrays.asList(new Word("Animals"), new Word("Daycare"))))
+					.setSummary("An example of a summary for daycare service in north Auckland")
+					.setContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+					.buildDocument();
+			
+			Document catCafeDocument = new DocumentBuilder()
+					.setTitle("Cat Cafes in south Auckland")
+					.setKeywords(new ArrayList<Word>(Arrays.asList(new Word("Cats"), new Word("Cafe"), new Word("Animals"))))
+					.setSummary("An example of a summary for cat cafes in south Auckland")
+					.setContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+					.buildDocument();
+			
+			Document dogTrainingDocument = new DocumentBuilder()
+					.setTitle("Dog training in west Auckland")
+					.setKeywords(new ArrayList<Word>(Arrays.asList(new Word("Dogs"), new Word("Training"), new Word("Animals"))))
+					.setSummary("An example of a summary for dog training in central Auckland")
+					.setContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+					.buildDocument();
+
+			Document baristaTrainingDocument = new DocumentBuilder()
+					.setTitle("Barista training in west Auckland")
+					.setKeywords(new ArrayList<Word>(Arrays.asList(new Word("Barista"), new Word("Training"))))
+					.setSummary("An example of a summary for Barista training in east Auckland")
+					.setContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+					.buildDocument();
+			
+			Document parrotDaycareDocument = new DocumentBuilder()
+					.setTitle("Parrot daycare and training in west Auckland")
+					.setKeywords(new ArrayList<Word>(Arrays.asList(new Word("Parrots"), new Word("Daycare"), new Word("Animals"), new Word("Training"))))
+					.setSummary("An example of a summary for Parrot daycare and training in west Auckland")
+					.setContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+					.buildDocument();
+			
+			
+			List<Document> documents = new ArrayList<Document>();
+			documents.add(dogWalkingRoskillDocument);
+			documents.add(animalDayCareDocument);
+			documents.add(catCafeDocument);
+			documents.add(dogTrainingDocument);
+			documents.add(baristaTrainingDocument);
+			documents.add(parrotDaycareDocument);
+			return documents;
+		}
+
 	private List<Document> getDocumentsOnKeywords(List<String> words) {
 		List<String> keywords = new ArrayList<String>();
 		for (String s : words) {
